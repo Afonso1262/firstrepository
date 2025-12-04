4ee81669174be315feaaadb7cba3fcd002055dd6
Afonso1262 <1262@iai.pt>
2025-12-04 11:56:19 +0000

    e

---
+++ Jogo 2/app.js
@@ -0,0 +1,20 @@
+let adivinha = prompt('Eu cresço apesar de não estar vivo. Não tenho pulmões, mas para viver preciso de ar. A água, mesmo que eu não tenha boca, me mata. O que eu sou?')
+let resposta_v1 = "Fogo"
+let resposta_v2 = "O Fogo"
+let resposta_v3 = "O fogo"
+let resposta_v4 = "fogo"
+let resposta_v5 = "o fogo"
+
+let ganhou = false;
+
+console.log(adivinha)
+console.log(resposta_v1, resposta_v2, resposta_v3, resposta_v4, resposta_v5)
+while (!ganhou) {
+    if (adivinha === resposta_v1 || adivinha === resposta_v2 || adivinha === resposta_v3 || adivinha === resposta_v4 || adivinha === resposta_v5) {
+        alert('YAY! Ganhaste!!')
+        ganhou = true;
+    } else {
+        alert('Dica: estas mais quente!')
+        adivinha = prompt('Eu cresço apesar de não estar vivo. Não tenho pulmões, mas para viver preciso de ar. A água, mesmo que eu não tenha boca, me mata. O que eu sou?')
+    }
+}
